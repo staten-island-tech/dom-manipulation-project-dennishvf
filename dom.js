2a1a5efd3a31f4476dc74144332e23cab7919495
dennishvf <dennisk38@nycstudents.net>
2023-10-19 09:24:44 -0400

    vdsdsg

---
+++ dom.js
@@ -0,0 +1,7 @@
+const DOMselectors= {
+    form: document.querySelector("form"),
+    Animalname: document.querySelector(".animal-name"),
+    h2: document.querySelector("h2")
+};
+console.log(DOMselectors.Animalname)
+function()
