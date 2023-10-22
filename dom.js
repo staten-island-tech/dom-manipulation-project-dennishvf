be7918ba896d3df7045bcd98a5934cee953ca388
dennishvf <dennisk38@nycstudents.net>
2023-10-22 14:33:29 -0400

    ,,

--- dom.js
+++ dom.js
@@ -1,3 +1,14 @@
-const domselectors = {
-    
-}
+const DOMselectors = {
+    form: document.querySelector("form"),
+    namesubmit: document.querySelector(".animal-name"),
+    h2s: document.querySelector("h2")
+};
+console.log(DOMselectors.h2s)
+
+DOMselectors.form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(DOMselectors.namesubmit.value);
+    DOMselectors.h2s.forEach(
+        (el) => (el.textContent = DOMselectors.namesubmit.value)
+    );
+});
