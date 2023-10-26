004dedc52955181549db342f28268e7bfa64d2f4
dennishvf <dennisk38@nycstudents.net>
2023-10-26 09:22:56 -0400

    jhgjhgjhgjg

--- dom.js
+++ dom.js
@@ -1,14 +1,15 @@
 const DOMselectors= {
     form: document.getElementById("form"),
     namesubmit: document.getElementById("animal-name"),
-    h2s: document.querySelectorAll(".card-title"),
+    title: document.querySelectorAll(".card-title"),
+    button: document.getElementById("btn"),
 };
-console.log(DOMselectors.h2s);
+console.log(DOMselectors.title);
 
-DOMselectors.form.addEventListener("submit", function (event) {
+DOMselectors.form.addEventListener("submit", (event) => {
     event.preventDefault();
     console.log(DOMselectors.namesubmit.value);
-    DOMselectors.h2s.forEach((hed) => (hed.textContent = DOMselectors.namesubmit.value));
+    DOMselectors.title.forEach((hed) => (hed.textContent = DOMselectors.namesubmit.value));
 });
 
 function makecard() {
