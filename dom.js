cb8333469b37135c07750c3b8b74d6c5dc4f5d1b
dennishvf <dennisk38@nycstudents.net>
2023-10-26 15:43:20 -0400

    ,mn.,n.

--- dom.js
+++ dom.js
@@ -1,15 +1,29 @@
 const DOMselectors= {
     form: document.getElementById("form"),
-    namesubmit: document.getElementById("animal-name"),
+    Aninameform: document.getElementById("animal-name"),
+    Aniimgform: document.getElementById("image"),
+    Anidescform: document.getElementById("Type-of-diet"),
     title: document.querySelectorAll(".card-title"),
+    image: document.querySelectorAll(".pic"),
+    desc: document.querySelectorAll(".animal-desc"),
     button: document.getElementById("btn"),
 };
 console.log(DOMselectors.title);
+console.log(DOMselectors.image);
+console.log(DOMselectors.desc);
 
 DOMselectors.form.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(DOMselectors.namesubmit.value);
-    DOMselectors.title.forEach((hed) => (hed.textContent = DOMselectors.namesubmit.value));
+    console.log(DOMselectors.Aninameform.value);
+    DOMselectors.title.forEach((hed) => (hed.textContent = DOMselectors.Aninameform.value));
+});
+DOMselectors.form.addEventListener("submit", (event2) => {
+    console.log(DOMselectors.Aniimgform.value);
+    DOMselectors.image.forEach((pict) => (pict.textContent = DOMselectors.Aniimgform.value));
+});
+DOMselectors.form.addEventListener("submit",(event3) => {
+    console.log(DOMselectors.Anidescform.value);
+    DOMselectors.desc.forEach((des) => (des.textContent = DOMselectors.Anidescform.value));
 });
 
 function makecard() {
